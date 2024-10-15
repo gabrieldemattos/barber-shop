ab99e368f22d53fe28b7d8db1544d577ed499e25
Gabriel de Mattos <mattos_95@hotmail.com>
2024-10-15 15:13:33 -0300

    chore: update shadcn theme

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="h-screen w-full items-center bg-red-500">
+    <div className="h-screen w-full items-center">
       <h1 className="text-center text-3xl">Olá</h1>
     </div>
   )
